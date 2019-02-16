1404d593313a23a850da37afdd9be352009e3c81
TopDev <top.dev20k@gmail.com>
2019-02-16 18:00:38 +0100

    create vue project

---
+++ vue-cryptoraves/cryptoraves/src/components/Landing.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  data () {
+    return {
+      title: 'It\'s Crypto You Can Tweet!'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import './../node_modules/foo/bar.css';
+</style>
