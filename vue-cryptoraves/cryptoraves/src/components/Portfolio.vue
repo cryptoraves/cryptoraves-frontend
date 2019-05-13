4b396466f6fc4985253bd1da6c35dbe15d2a7663
bryan <bryan@residentialdynamics.com>
2019-05-13 15:37:21 -0400

    new js

--- vue-cryptoraves/cryptoraves/src/components/Portfolio.vue
+++ vue-cryptoraves/cryptoraves/src/components/Portfolio.vue
@@ -45,7 +45,7 @@
                                                 <tr v-for="(item,index) in tableRows" :index="index" :key="item.txnId">
                                                     <th class="link" v-on:click="goTransaction(item.txnHash)" scope="row"><b>{{item.txnHash | truncate}}</b></th>
                                                     <td class="link" v-on:click="goAnother(item.from)"><b>{{item.from}}</b></td>
-                                                    <td class="link"><a>{{item.linkToContent}}</a></td>
+                                                    <td class="link"><a href="{{item.linkToContent}}">T</a></td>
                                                     <td>{{item.amount | comma}}</td>
                                                     <td class="link" v-on:click="goAnother(item.to)"><b>{{item.to}}</b></td>
                                                     <td>{{item.date}}</td>
