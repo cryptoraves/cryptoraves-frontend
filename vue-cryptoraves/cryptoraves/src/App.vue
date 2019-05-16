4e86d4137e6b16c5c5534cb979751f76b29123e4
bryan <bryan@residentialdynamics.com>
2019-05-16 10:41:33 -0400

    ethereum logo removed

--- vue-cryptoraves/cryptoraves/src/App.vue
+++ vue-cryptoraves/cryptoraves/src/App.vue
@@ -6,7 +6,13 @@
                 <div class="col-sm-12 col-lg-2">
                     <div class="logo-area">
                       <router-link to="/"><img src="./assets/img/cryptoraves_Horozontal Alignment_Full Color_JPG.png" alt=""></router-link>  
-                                           
+                      <img src="./assets/img/built-on-ethereum.png" alt="">                     
+                    </div>
+                </div> 
+                <div class="col-sm-12 col-lg-1">
+                    <div class="logo-area">
+                      
+                      <img src="./assets/img/built-on-ethereum.png" alt="">                     
                     </div>
                 </div>             
                 <div class="col-sm-12 col-lg-10 text-right">
