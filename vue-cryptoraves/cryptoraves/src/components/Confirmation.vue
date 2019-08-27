4a613ffdbb68c046fd8405ee46f97e7c3ce3f3c3
cartosys <plaster1@gmail.com>
2019-08-27 11:28:22 -0400

    hover message

--- vue-cryptoraves/cryptoraves/src/components/Confirmation.vue
+++ vue-cryptoraves/cryptoraves/src/components/Confirmation.vue
@@ -7,12 +7,12 @@
                 <div class="row">
                     <div class="col-12 col-md-12 align-self-center">
                         <div class="welcome-right">                        
-                            <div id="hoverEl" class="welcome-text">
+                            <div  class="welcome-text">
                                 <h1>It's Crypto You Can Tweet!</h1>
                                 <!-- <h2 class="blinking">Live On Testnet Only!!! Tokens Will Be Deleted Before Alpha Launch!</h2> -->
                                 <h2>Transaction Confirmed.</h2>
-                                <table @mouseover="mouseOver" class="table" style="border: none"><td style="border: none" class="link" v-on:click="goBlock()"><b><h4>{{txnID}}</h4></b></td></table>
-                                 <p v-show="active">Click to see on block explorer</p>
+                                <table @mouseover="upHere = true" @mouseleave="upHere = false" class="table" style="border: none"><td style="border: none" class="link" v-on:click="goBlock()"><b><h4>{{txnID}}</h4></b></td></table>
+                                 <p v-show="upHere">Click to see on block explorer</p>
                                 
                             </div>                       
                         </div>
@@ -80,6 +80,7 @@ export default {
           txnId: '',
           txnID: '',
           tableRows: [],
+          upHere : false
         }
     },
     created() {
@@ -115,15 +116,4 @@ export default {
         }
     }
 }
-var demo = new Vue({
-    el: '#hoverEl',
-    data: {
-        active: false
-    },
-    methods: {
-        mouseOver: function(){
-            this.active = !this.active;   
-        }
-    }
-});
 </script>
