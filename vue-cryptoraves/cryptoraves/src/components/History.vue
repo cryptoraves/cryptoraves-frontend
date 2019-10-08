a8cb40193f1ca44a642685dd700a18518f7f5385
cartosys <plaster1@gmail.com>
2019-10-08 12:34:13 -0400

    make live

--- vue-cryptoraves/cryptoraves/src/components/History.vue
+++ vue-cryptoraves/cryptoraves/src/components/History.vue
@@ -37,6 +37,7 @@
                                                 <th scope="col">From</th>
                                                 <th scope="col">Link To Tweet</th>
                                                 <th scope="col">Amount</th>
+                                                <th scope="col">io</th>
                                                 <th scope="col">To</th>
                                                 <th scope="col">Date</th>
                                                 </tr>
@@ -47,6 +48,7 @@
                                                     <td class="link" v-on:click="goAnother(item.userFrom)"><b>{{item.userFrom}}</b></td>
                                                     <td class="link" v-on:click="goTweet(item.linkToContent)"><img v-bind:src="'/static/img/twittersmall.png'" /> </td>
                                                     <td>{{item.amount | comma}}</td>
+                                                    <td>{{item.inOut}}</td>
                                                     <td class="link" v-on:click="goAnother(item.userTo)"><b>{{item.userTo}}</b></td>
                                                     <td>{{item.date}}</td>
                                                 </tr>                                                                                            
