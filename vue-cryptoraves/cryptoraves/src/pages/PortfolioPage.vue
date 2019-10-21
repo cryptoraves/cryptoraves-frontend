c651007c5dce2bdbc344b13629eda9be453b760d
cartosys <plaster1@gmail.com>
2019-10-21 09:41:24 -0400

    again

--- vue-cryptoraves/cryptoraves/src/pages/PortfolioPage.vue
+++ vue-cryptoraves/cryptoraves/src/pages/PortfolioPage.vue
@@ -140,6 +140,7 @@ export default {
   beforeRouteUpdate(to, from, next) {
     // just use `this`
     this.user = to.query.user;
+    this.getPortfolio(this.user, 0);
     next();
   },
   methods: {
